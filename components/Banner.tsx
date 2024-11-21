fb7c63f60a0a8dbc1d061b73072dcb7330853bb4
Jamal Riley <73957695+jamalmriley@users.noreply.github.com>
2024-11-21 16:35:36 -0600

    Update Banner.tsx
    - Update Banner with a useEffect and simplified functionality to fix hydration issue.

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -14,25 +14,19 @@ export default function Banner({
   text: string;
   publishDate: Date;
 }) {
-  let value;
-  if (typeof window !== "undefined") {
-    value = localStorage.getItem("lastConfirmationDate") || "";
-  }
-  const [now, setNow] = useState(new Date());
-  const [lastConfirmationDate, setLastConfirmationDate] = useState(
-    value || now.toUTCString()
-  );
-  const saveToLocalStorage = () => {
-    const newVal = new Date().toUTCString();
-    localStorage.setItem("lastConfirmationDate", newVal);
-    setLastConfirmationDate(newVal);
+  const [showBanner, setShowBanner] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleCloseBanner = () => {
+    setShowBanner(false);
+    localStorage.setItem("bannerDismissed", "true"); // Store dismissal in local storage
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setNow(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
+    if (localStorage.getItem("bannerDismissed") === "true") {
+      setShowBanner(false);
+    }
+    setIsLoading(false);
   }, []);
 
   return (
@@ -45,37 +39,41 @@ export default function Banner({
       The user has never pressed the "X" button.)
       AND (&&)
       (The publish date has passed.)
-      */}
-      {((lastConfirmationDate === undefined
+
+      ((lastConfirmationDate === undefined
         ? new Date()
         : new Date(lastConfirmationDate) <= publishDate) ||
         value === "") &&
-        now >= publishDate && (
-          <div className="flex justify-between items-center w-full h-20 px-10 py-5 bg-emerald-50 dark:bg-emerald-950">
-            <div className="flex items-center gap-3">
-              <div className="w-9 h-9 bg-emerald-500 dark:bg-emerald-400 rounded-full flex items-center justify-center">
-                <CiBullhorn className="w-5 h-5 text-emerald-50 dark:text-emerald-950" />
-              </div>
-              <div className="flex flex-col gap-0.5">
-                <span className="text-lg font-bold text-emerald-950 dark:text-emerald-50">
-                  {header}
-                </span>
-                <span className="text-xs font-medium text-emerald-800 dark:text-emerald-100">
-                  {text}
-                </span>
-              </div>
+        now >= publishDate
+
+      */}
+
+      {showBanner && !isLoading && (
+        <div className="flex justify-between items-center w-full h-20 px-10 py-5 bg-emerald-50 dark:bg-emerald-950">
+          <div className="flex items-center gap-3">
+            <div className="w-9 h-9 bg-emerald-500 dark:bg-emerald-400 rounded-full flex items-center justify-center">
+              <CiBullhorn className="w-5 h-5 text-emerald-50 dark:text-emerald-950" />
             </div>
-            <Button
-              type="submit"
-              variant="ghost"
-              size="icon"
-              className="rounded-full min-w-7 w-7 h-7"
-              onClick={() => saveToLocalStorage()}
-            >
-              <IoMdClose />
-            </Button>
+            <div className="flex flex-col gap-0.5">
+              <span className="text-lg font-bold text-emerald-950 dark:text-emerald-50">
+                {header}
+              </span>
+              <span className="text-xs font-medium text-emerald-800 dark:text-emerald-100">
+                {text}
+              </span>
+            </div>
           </div>
-        )}
+          <Button
+            type="submit"
+            variant="ghost"
+            size="icon"
+            className="rounded-full min-w-7 w-7 h-7"
+            onClick={() => handleCloseBanner()}
+          >
+            <IoMdClose />
+          </Button>
+        </div>
+      )}
     </>
   );
 }
